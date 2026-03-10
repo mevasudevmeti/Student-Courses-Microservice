b27c007b5a4931e5499acf3489fbbabe323f55a4
vasu <vasu.c.meti@gmail.com>
2026-03-10 09:56:48 +0000

    feat(00): student service structure

---
+++ student-service/src/main/java/com/lbu/student_service/StudentServiceApplication.java
@@ -0,0 +1,15 @@
+package com.lbu.student_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StudentServiceApplication.class, args);
+		System.out.println("Student Service Application Started");
+	}
+
+}
