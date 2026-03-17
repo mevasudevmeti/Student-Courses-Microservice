109887fae1b19cbe3935780c9a0878dd8dee8efd
vasu <vasu.c.meti@gmail.com>
2026-03-17 11:36:50 +0000

    feat(00): Student entity

---
+++ student-service/src/main/java/com/lbu/student_service/entities/Student.java
@@ -0,0 +1,25 @@
+package com.lbu.student_service.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+
+    @Column(name = "firstName", length = 150, nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", length = 150, nullable = false)
+    private String lastName;
+    @Column(name = "email", length = 150, nullable = false, unique = true)
+    private String email;
+
+}
